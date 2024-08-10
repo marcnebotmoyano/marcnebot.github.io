e8f18eb663b3a35d2d068e2bf292563469da34f2
marcnebotmoyano <marcnebotmoyano@gmail.com>
2024-08-11 00:51:21 +0200

    Improvement commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Welcome to Marc Nebot\'s Website');
+    loadProjects();
 });
+
+function loadProjects() {
+    const projects = [
+        { title: 'Generative Artificial Tabular Data Methods', description: 'TFG-FIB-GenerativeArtificialTabularDataMethods' }
+    ];
+
+    const projectsList = document.getElementById('projects-list');
+    projects.forEach(project => {
+        const projectElement = document.createElement('div');
+        projectElement.classList.add('project');
+        projectElement.innerHTML = `
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+        `;
+        projectsList.appendChild(projectElement);
+    });
+}
